dbc76b448249ee6899f3a379a0efa877d9a0c6d3
Pavel <kpo89@inbox.ru>
2022-09-19 23:11:45 +0300

    add Task11,Task12,Task14,Task16

---
+++ Task11/Program.cs
@@ -0,0 +1,13 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 999);
+Console.WriteLine ($"Случайное число из отрезка: {number}");
+int first = number / 100;
+int third = number % 10;
+int result = first*10 + third;
+Console.WriteLine ($"Искомое число: {result}");
---
+++ Task14/Program.cs
@@ -0,0 +1,21 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+
+Console.Write("Введите первый делитель: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второй делитель: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите делимое: ");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+bool Multiple (int num, int numD)
+{
+    return ((num % numD) == 0);
+}
+if (Multiple(numC,numA) && Multiple(numC,numB)) Console.WriteLine("кратно обоим");
+    else Console.WriteLine("НЕ кратно обоим");
